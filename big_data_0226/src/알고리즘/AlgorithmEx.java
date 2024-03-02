af7eee2f49af0e7077edfc0a58c1882164da1f5a
qarksangwon <0629psw@gmail.com>
2024-03-02 18:52:55 +0900

    재귀 알고리즘

---
+++ big_data_0226/src/알고리즘/AlgorithmEx.java
@@ -0,0 +1,40 @@
+package 알고리즘;
+
+public class AlgorithmEx {
+    public static void main(String[] args) {
+        System.out.println(fibonacci(5));
+
+    }
+
+    //재귀 함수
+    //조건이 달성할 때 까지 자신(함수)를 다시 호출 하는 Algorithm.
+    //등차 수열 : f(n) = f(n-1) + a
+    //등비 수열 : f(n) = f(n-1) * a
+    //factorial : f(n) = f(n-1)*n  >> n보다 작거나 같은 모든 양의 정수의 곱.
+    //fibonacci : f(n) = f(n-1)+f(n-2) >> 0항을 0, 1항을 1로 두고, 둘째 번 항부터 그 뒤의 모든 항이 바로 앞 두 항의 덧셈.
+    public static int factorial(int n){
+        if(n == 1){
+            return 1;
+        }
+        else{
+            return factorial(n-1)*n;
+        }
+    }
+
+    public static int fibonacci(int n){
+        if(n<=1){
+            return n;
+        }else{
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+        // ex fibonacci(5)
+        // fibonacci(5) = fibonacci(4)+fibonacci(3) = 3 + 2 = 5
+        // fibonacci(4) = fibonacci(3)+fibonacci(2) = 2 + 1 = 3
+        // fibonacci(3) = fibonacci(2)+fibonacci(1) = 1 + 1 = 2
+        // fibonacci(2) = fibonacci(1)+fibonacci(0) = 1 + 0 = 1
+        // fibonacci(1) = 1
+        // fibonacci(0) = 0
+    }
+
+
+}
