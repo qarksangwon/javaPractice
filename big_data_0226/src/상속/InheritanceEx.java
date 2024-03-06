e324e71bbfc1a7d4214cdf6ea1f2e7d1d9bb313f
qarksangdol <0629psw@gmail.com>
2024-03-06 11:50:37 +0900

    객체 타입확인

--- big_data_0226/src/상속/InheritanceEx.java
+++ big_data_0226/src/상속/InheritanceEx.java
@@ -9,7 +9,17 @@ public class InheritanceEx {
         ((HouseDog)hd).sleep(4); // 이런식으로는 사용 가능.
         //하지만 애초에 new 생성자로 인스턴스를 부모로만들 시
         Dog d1 = new Dog();
+        HouseDog d2 = new HouseDog();
         // ((HouseDog)d1).sleep(); 이건 사용하지 못한다.
+
+        // 객체 타입을 확인 할 때에는 instanceof
+        // 자식->부모 타입을 만족하지만 부모 -> 자식 타입을 만족하지 못함.
+        // 즉 타입은 부모가 가진건 자식이 모두 가졌지만 자식이 가진건 부모가 없을 수 있기에
+        // 부모가 자식 타입인지 확인할 때에는 False 가 나오는 것.
+        System.out.println(d1 instanceof Dog);       // 부모 객체 -> 부모 타입 (T)
+        System.out.println(d1 instanceof HouseDog);  // 부모 객체 -> 자식 타입 (F)
+        System.out.println(d2 instanceof Dog);       // 자식 객체 -> 부모 타입 (T)
+        System.out.println(d2 instanceof HouseDog);  // 자식 객체 -> 자식 타입 (T)
 
     }
 }
