daecceb3727208155b1ccdea40942f69763c05fd
qarksangdol <0629psw@gmail.com>
2024-03-06 11:28:58 +0900

    final과static

---
+++ big_data_0226/src/파이널/Common.java
@@ -0,0 +1,14 @@
+package 파이널;
+
+public class Common {
+    final static String[] screenType = {"", "11인치", "12.9인치"};
+    final static String[] colorType = {"", "스페이스그레이", "실버"};
+    final static String[] memType = {"", "128BG", "256GB", "512GB", "1TB"};
+    final static String[] netType = {"", "Wi-Fi", "Wi-Fi+Cellular"};
+}
+
+class Constants {
+    public static final int MAX_VALUE = 100; // 정수 상수
+    public static final double PI = 3.14159; // 실수 상수
+    public static final String GREETING = "Hello, World!"; // 문자열 상수
+}
---
+++ big_data_0226/src/파이널/FinalStatic.java
@@ -0,0 +1,7 @@
+package 파이널;
+
+public class FinalStatic {
+    public static void main(String[] args) {
+        System.out.println(Common.screenType[1]);
+    }
+}
