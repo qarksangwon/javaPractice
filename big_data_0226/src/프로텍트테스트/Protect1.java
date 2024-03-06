86613efe41a27474a7baf9f011df1fb7b0751ef3
qarksangdol <0629psw@gmail.com>
2024-03-06 17:45:18 +0900

    프로텍트 연습

--- big_data_0226/src/프로텍트테스트/Protect1.java
+++ big_data_0226/src/프로텍트테스트/Protect1.java
@@ -1,12 +1,6 @@
 package 프로텍트테스트;
 
-public class Protect1 extends  Protect3 {
-    public static void main(String[] args) {
-        Protect1 pc1 = new Protect1();
-        System.out.println(pc1.val);
-    }
-}
-
-class Protect3 {
+public class Protect1 {
     protected int val = 10;
+
 }
--- big_data_0226/src/프로텍트테스트2/Protect2.java
+++ big_data_0226/src/프로텍트테스트2/Protect2.java
@@ -2,15 +2,22 @@ package 프로텍트테스트2;
 
 import 프로텍트테스트.Protect1;
 
-public class Protect2 {
+//프로텍트는 직접 상속받은 클래스 내에서는 사용이 가능하다.(패키지가 달라도)
+// 하지만 상속받은 클래스를 다른 클래스에서 인스턴스로 만들어 사용은 불가하다
+
+public class Protect2 extends Protect1 {
     public static void main(String[] args) {
-       Test test = new Test();
-       //System.out.println(test.val); 불가능
-        Protect1 test2 = new Protect1();
-       // System.out.println(test2.val); 불가능
+        Protect2 pc = new Protect2();
+        System.out.println(pc.val);
+        Protect3 pc2 = new Protect3();
+        // System.out.println(pc2.val); 즉 이렇게 상속받은 클래스 객체를 만들어서 접근은 X
+        pc2.message(); //하지만 내장함수를 사용하면 이것도 가능
     }
 }
 
-class Test extends Protect1{
-
+class Protect3 extends Protect1{
+    void message(){
+        System.out.println(this.val);
+    }
 }
+
