6514133917aa727f8483961ecf5e0c4c6f9b5f6c
qarksangdol <0629psw@gmail.com>
2024-03-11 12:13:43 +0900

    인터페이스 다운캐스팅

--- big_data_0226/src/인터페이스/Main.java
+++ big_data_0226/src/인터페이스/Main.java
@@ -4,28 +4,29 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Animal[] animals = {new Dog("말티즈"),new Dog("푸들")
-//                ,new Cat("고영희"),new Dog("말티푸")
-//                , new Cat("김영희"),new Cow("워낭소리"), new Cow("황소")};
-//        for(Animal ani : animals){
-//            if(ani instanceof Dog){
+        Animal[] animals = {new Dog("말티즈"),new Dog("푸들")
+                ,new Cat("고영희"),new Dog("말티푸")
+                , new Cat("김영희"),new Cow("워낭소리"), new Cow("황소")};
+        for(Animal ani : animals){
+            if(ani instanceof Dog){
 //                ani.sound();
 //                ((Dog)ani).yum();
-//                System.out.println("강아지는 너무 " + ani.type);
-//            }
-//            else if(ani instanceof  Cat) System.out.println("고양이도 " + ani.type);
-//        }
-        Animal ani;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("키우고 싶은 동물: [1]강아지 [2]고양이 [3]소");
-        int num = sc.nextInt();
-        switch (num){
-            case 1 : ani = new Dog("뽀삐"); break;
-            case 2 : ani = new Cat("고영희"); break;
-            case 3 : ani = new Cow("워낭소리"); break;
-            default: System.out.println("그런 동물은 없어~"); return;
+                Dog dog = (Dog)ani;
+                dog.yum();
+                System.out.println(dog.name + dog.type);
+            }
         }
-        ani.sound();
+//        Animal ani;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("키우고 싶은 동물: [1]강아지 [2]고양이 [3]소");
+//        int num = sc.nextInt();
+//        switch (num){
+//            case 1 : ani = new Dog("뽀삐"); break;
+//            case 2 : ani = new Cat("고영희"); break;
+//            case 3 : ani = new Cow("워낭소리"); break;
+//            default: System.out.println("그런 동물은 없어~"); return;
+//        }
+//        ani.sound();
     }
 }
 
