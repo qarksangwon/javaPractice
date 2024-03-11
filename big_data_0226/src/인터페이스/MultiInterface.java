797511851b1db2a79fe93ccfcbb2c38fd674a2fa
qarksangdol <0629psw@gmail.com>
2024-03-11 15:38:45 +0900

    다중상속 및 다이아몬드 문제

---
+++ big_data_0226/src/인터페이스/MultiInterface.java
@@ -0,0 +1,51 @@
+package 인터페이스;
+
+public class MultiInterface {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        Buy buyer = customer;
+        buyer.buy();
+        buyer.order();
+
+        Sell seller = customer;
+        seller.sell();
+        seller.order();
+
+        Customer customer1 = (Customer) seller;
+        customer1.buy();
+        customer1.sell();
+        customer1.order();
+    }
+}
+
+interface Sell{
+    void sell();
+    default void order(){
+        System.out.println("판매 주문");
+    }
+}
+interface Buy{
+    void buy();
+    default void order(){
+        System.out.println("구매 주문");
+    }
+}
+
+class Customer implements Buy, Sell{
+
+    @Override
+    public void sell() {
+        System.out.println("고객이 판매하기");
+    }
+
+    @Override
+    public void buy() {
+        System.out.println("고객이 구매하기");
+    }
+
+    @Override
+    public void order() {
+        System.out.println("주문");
+    }
+
+}
