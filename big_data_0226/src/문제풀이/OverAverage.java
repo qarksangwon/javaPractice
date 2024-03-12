5d4cb442e3e2873f2f5c548003f94dbfd5d90eb7
qarksangdol <0629psw@gmail.com>
2024-03-12 17:27:28 +0900

    평균 넘는 학생 비율찾기 문제

---
+++ big_data_0226/src/문제풀이/OverAverage.java
@@ -0,0 +1,55 @@
+package 문제풀이;
+// 첫 줄에 테스트 케이스 개수 C 주어짐
+// 두번 째 줄부터 각 테스트 케이스마다 학생 수 (1<= N <=1000, N은 정수)
+// 학생수 N이 주어지고 N명의 점수가 주어짐. 점수는 0~100
+// 각 케이스마다 한 줄씩 평균을 넘는 학생의 비율을 반올림 해서 소수점 셋째 자리까지 출력
+//입력
+//5
+//5 50 50 70 80 100
+//7 100 95 90 80 70 60 50
+//3 70 90 80
+//3 70 90 81
+//9 100 99 98 97 96 95 94 93 91
+//출력
+//40.000%
+//57.143%
+//33.333%
+//66.667%
+//55.556%
+
+
+
+import java.util.Scanner;
+
+public class OverAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuffer rst = new StringBuffer();
+        int tCnt = sc.nextInt();
+        for(int i=0; i<tCnt; i++){
+            int current = sc.nextInt();
+            int[] currents = new int[current];
+            for(int j=0; j<current; j++){
+                currents[j] = sc.nextInt();
+            }
+            rst.append(ratio(current,currents)+"% \n");
+        }
+        System.out.println(rst);
+    }
+
+    static double ratio(int stdCnt, int[] score){
+        int cnt = 0;
+        int sum = 0;
+        for(int e : score) sum +=e;
+        int avg = sum/stdCnt;
+        for(int c : score){
+            if(c > avg) cnt++;
+        }
+        double ratio = (double) Math.round(((100 * cnt) / (double) stdCnt) * 1000) /1000;
+        return ratio;
+    }
+
+}
+// 총인원 : 100 / 평균넘는인원 : 비율
+// 비율 x 총인원 = 100 x 평균넘는인원
+// 비율 = (100*평균넘는인원)/총인원
