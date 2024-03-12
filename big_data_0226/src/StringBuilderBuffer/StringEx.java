16cb9569a7fd5029cb7f4bf5940f66c9372bdbb3
qarksangdol <0629psw@gmail.com>
2024-03-12 10:08:58 +0900

    String builder와 buffer

---
+++ big_data_0226/src/StringBuilderBuffer/StringEx.java
@@ -0,0 +1,20 @@
+package StringBuilderBuffer;
+
+public class StringEx {
+    public static void main(String[] args) {
+        StringBuilder sbl = new StringBuilder();
+        sbl.append("hello");
+        sbl.append(" ");
+        sbl.append("jump to java");
+        String result = sbl.toString();
+        System.out.println(result);
+
+        System.out.println();
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("jump to java");
+        sb.insert(0, "hello ");
+        System.out.println(sb);
+        System.out.println(sb.substring(0, 4));
+    }
+}
