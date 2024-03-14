7e994393cdff02f5f82a3ba5dc0ac39cbed6ac06
qarksangdol <0629psw@gmail.com>
2024-03-14 10:02:36 +0900

    properties 연습

---
+++ big_data_0226/src/map/PropertiesEx.java
@@ -0,0 +1,29 @@
+package map;
+// Properties 란 키와 값을 String 타입으로 제한한다.
+// 제한하는 이유는 설정 정보 등을 파일에 쓰기 위해서 이다.
+// Properties 를 사용하는 이유는
+// 환경 설정이나 뭔가 중요한 파일을 만들어놓고 해당 파일을 숨기기 위해
+// 위의 해당하는 경우에 사용하게 된다고 한다.
+
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.Properties;
+
+public class PropertiesEx {
+    public static void main(String[] args) throws IOException {
+        Properties properties = new Properties();
+        String path = PropertiesEx.class.getResource("./database.properties").getPath();
+        path = URLDecoder.decode(path, "utf-8");
+        properties.load(new FileReader(path));
+        String driver = properties.getProperty("driver");
+        String url = properties.getProperty("url");
+        String username = properties.getProperty("username");
+        String password = properties.getProperty("password");
+        System.out.println("drive : " + driver);
+        System.out.println("url : " + url);
+        System.out.println("username : " + username);
+        System.out.println("password : " + password);
+    }
+}
